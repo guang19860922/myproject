4100d691dd484c16cc654b854679b2592973a617
guang19860922 <guang19860922@gmail.com>
2020-07-11 17:06:35 +0800

    java and kotlin variable, data type

---
+++ src/main/java/com/guang/Person.java
@@ -0,0 +1,7 @@
+package com.guang;
+
+public class Person {
+    public void hello(){
+        System.out.println("Hello World");
+    }
+}
---
+++ src/main/kotlin/com/guang/Hello.kt
@@ -0,0 +1,17 @@
+package com.guang
+
+fun main(args: Array<String>) {
+//    println("Hello Kotlin!")
+    val h = Human()
+    h.hello()
+    var age = 19;
+    age = 20;
+    var weight = 66.5f
+    var name : String
+    name = "Guang"
+}
+class Human{
+    fun hello(){
+        println("Hello World")
+    }
+}
